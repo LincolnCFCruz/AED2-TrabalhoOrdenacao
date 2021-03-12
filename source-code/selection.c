5a4f3a49274c22e4a69fba8d394c37ff19f984fb
LincolnCFCruz <79378712+LincolnCFCruz@users.noreply.github.com>
2021-03-11 23:46:36 -0300

    Update selection.c

--- source-code/selection.c
+++ source-code/selection.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
   
-void swap(int *xp, int *yp) { 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
+void swap(int *n1, int *n2) { 
+    int temp = *n1; 
+    *n1 = *n2; 
+    *n2 = temp; 
 } 
   
 void selectionSort(int arr[], int n) { 
