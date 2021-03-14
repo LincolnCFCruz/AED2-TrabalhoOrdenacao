265c08f28cfda43e92c78e23080cd074c563ecdf
LincolnCFCruz <79378712+LincolnCFCruz@users.noreply.github.com>
2021-03-13 21:43:22 -0300

    Update merge.c

--- source-code/merge.c
+++ source-code/merge.c
@@ -9,7 +9,6 @@ void merge(int arr[], int l, int m, int r) {
  
     int L[n1], R[n2];
  
-
     for (i = 0; i < n1; i++){
         L[i] = arr[l + i];
     }
@@ -32,7 +31,6 @@ void merge(int arr[], int l, int m, int r) {
         k++;
     }
  
-
     while (i < n1) {
         arr[k] = L[i];
         i++;
